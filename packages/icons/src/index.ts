d28e01bf14e38a92a69ea87f9e45b840093282c2
Clement Creusat <clement.creusat@gmail.com>
2023-11-25 19:50:23 +0100

    fix: rollback icons

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,4 +1,3 @@
-export { default as Attachment } from "./Attachment";
 export { default as Audio } from "./Audio";
 export { default as Block } from "./Block";
 export { default as BurgerMenu } from "./BurgerMenu";
