7bce8be340d9c9b5c670cdc11f60051c5ce2922b
Clement Creusat <clement.creusat@gmail.com>
2023-11-25 19:05:25 +0100

    feat: clean package icon

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,4 +1,3 @@
-export { default as AlertTriangle } from "./AlertTriangle";
 export { default as Applications } from "./Applications";
 export { default as Attachment } from "./Attachment";
 export { default as Audio } from "./Audio";
