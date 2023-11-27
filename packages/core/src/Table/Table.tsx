789716e302b02518e58ab41cfc63effd1b3d59ee
Clement Creusat <clement.creusat@gmail.com>
2023-11-27 16:31:21 +0100

    feat: add table component

---
+++ packages/core/src/Table/Table.tsx
@@ -0,0 +1,26 @@
+import { TableProps } from "./TableProps";
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Table = ({
+  primary = false,
+  size = "medium",
+  backgroundColor,
+  label,
+  ...props
+}: TableProps) => {
+  const mode = primary ? "btn-primary" : "btn-secondary";
+  return (
+    <button
+      type="button"
+      className={["btn", `btn-${size}`, mode].join(" ")}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {label}
+    </button>
+  );
+};
+
+export default Table;
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,5 @@
 // Components
 export { default as Accordion } from "./Accordion/Accordion";
 export { default as Button } from "./Button/Button";
+export { default as Table } from "./Table/Table";
 export { default as Combobox } from "./Combobox/Combobox";
