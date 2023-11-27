2ba1aa0f5d92b42be3f848eb1eb459fcf55e490b
Clement Creusat <clement.creusat@gmail.com>
2023-11-27 14:03:11 +0100

    fix: remove icon burger menu

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,4 +1,3 @@
-export { default as BurgerMenu } from "./BurgerMenu";
 export { default as Calendar } from "./Calendar";
 export { default as CalendarLight } from "./CalendarLight";
 export { default as Camera } from "./Camera";
