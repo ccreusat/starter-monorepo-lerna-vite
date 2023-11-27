8c0f720276113052962e01bbb63f2e44a6805648
Clement Creusat <clement.creusat@gmail.com>
2023-11-27 17:48:33 +0100

    fix: remove icons check

--- packages/icons/src/index.ts
+++ packages/icons/src/index.ts
@@ -1,4 +1,3 @@
-export { default as Check } from "./Check";
 export { default as Checklist } from "./Checklist";
 export { default as Clock } from "./Clock";
 export { default as Close } from "./Close";
