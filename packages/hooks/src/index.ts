020d0c0e3154c9eb1ffabe69e614762a80d457ac
Clement Creusat <clement.creusat@gmail.com>
2023-11-27 11:18:27 +0100

    fix: cancel hooks

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,4 +1,4 @@
 export { default as useOnClickOutside } from "./use-click-outside";
-export { default as useTheme } from "./use-theme";
+// export { default as useTheme } from "./use-theme";
 /* export { default as useToggle } from "./use-toggle";
  */
